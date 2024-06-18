813718f1d22f81dd64067ac5375409e52b69e4ac
Scott <stsievert@users.noreply.github.com>
2024-06-17 20:40:47 -0400

    get ready to test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,22 @@
+echo "Copying tmux config..."
 cp .tmux.conf ~/
+
+echo "git aliases/shortcuts..."
+echo "alias g='git'" >> ~/.zshrc
+echo "alias g='git'" >> ~/.bashrc
+echo "[alias]\n  s = status\n  c = commit\n  a = add" >> ~/.gitconfig
+
+echo "Copying nvim config..."
 cp -r nvim ~/.config/nvim
-nvim -c "PlugInstall" -c "qa"
+
+echo "Installing nvim..."
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mkdir ~/bin
+cp nvim.appimage ~/bin/nvim.appimage
+echo "export PATH=~/bin/:$PATH" >> ~/.zshrc
+echo "export PATH=~/bin/:$PATH" >> ~/.bashrc
+
+echo "Installing nvim plugins..."
+./nvim.appimage -c "PlugInstall" -c "qa"
 echo "now run 'bash nvim/pylisp.sh', might fail because of local directory structure. Look at docs in nvim/README.md"
